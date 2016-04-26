a27fa44ba4af2fe528b1d70f43a01849b742192a
Chad Williamson <chad@vynca.org>
2016-04-26 10:51:48 -0700

    use ConstMetrics rather than shared state with locks

--- mirth_exporter.go
+++ mirth_exporter.go
@@ -10,7 +10,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/log"
@@ -18,160 +17,123 @@ import (
 
 const namespace = "mirth"
 
+var (
+	up = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "up"),
+		"Was the last Mirth query successful.",
+		nil, nil,
+	)
+	channelsDeployed = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "channels_deployed"),
+		"How many channels are deployed.",
+		nil, nil,
+	)
+	channelsStarted = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "channels_started"),
+		"How many of the deployed channels are started.",
+		nil, nil,
+	)
+	messagesReceived = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "messages_received_total"),
+		"How many messages have been received (per channel).",
+		[]string{"channel"}, nil,
+	)
+	messagesFiltered = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "messages_filtered_total"),
+		"How many messages have been filtered (per channel).",
+		[]string{"channel"}, nil,
+	)
+	messagesQueued = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "messages_queued"),
+		"How many messages are currently queued (per channel).",
+		[]string{"channel"}, nil,
+	)
+	messagesSent = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "messages_sent_total"),
+		"How many messages have been sent (per channel).",
+		[]string{"channel"}, nil,
+	)
+	messagesErrored = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "messages_errored_total"),
+		"How many messages have errored (per channel).",
+		[]string{"channel"}, nil,
+	)
+)
+
 type Exporter struct {
-	sync.Mutex
 	jarPath, configPath string
-	statLines           []string
-	up                  prometheus.Gauge
-	channelsDeployed    prometheus.Gauge
-	channelsStarted     prometheus.Gauge
-	messagesReceived    *prometheus.CounterVec
-	messagesFiltered    *prometheus.CounterVec
-	messagesQueued      *prometheus.GaugeVec
-	messagesSent        *prometheus.CounterVec
-	messagesErrored     *prometheus.CounterVec
 }
 
 func NewExporter(mccliJarPath, mccliConfigPath string) *Exporter {
 	return &Exporter{
 		jarPath:    mccliJarPath,
 		configPath: mccliConfigPath,
-		up: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "up",
-			Help:      "Was the last Mirth query successful.",
-		}),
-		channelsDeployed: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "channels_deployed",
-			Help:      "How many channels are deployed.",
-		}),
-		channelsStarted: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "channels_started",
-			Help:      "How many of the deployed channels are started.",
-		}),
-		messagesReceived: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: namespace,
-				Name:      "messages_received_total",
-				Help:      "How many messages have been received (per channel).",
-			},
-			[]string{"channel"},
-		),
-		messagesFiltered: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: namespace,
-				Name:      "messages_filtered_total",
-				Help:      "How many messages have been filtered (per channel).",
-			},
-			[]string{"channel"},
-		),
-		messagesQueued: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      "messages_queued",
-				Help:      "How many messages are currently queued (per channel).",
-			},
-			[]string{"channel"},
-		),
-		messagesSent: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: namespace,
-				Name:      "messages_sent_total",
-				Help:      "How many messages have been sent (per channel).",
-			},
-			[]string{"channel"},
-		),
-		messagesErrored: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: namespace,
-				Name:      "messages_errored_total",
-				Help:      "How many messages have errored (per channel).",
-			},
-			[]string{"channel"},
-		),
 	}
 }
 
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.up.Desc()
-	ch <- e.channelsDeployed.Desc()
-	ch <- e.channelsStarted.Desc()
-	e.messagesReceived.Describe(ch)
-	e.messagesFiltered.Describe(ch)
-	e.messagesQueued.Describe(ch)
-	e.messagesSent.Describe(ch)
-	e.messagesErrored.Describe(ch)
+	ch <- up
+	ch <- channelsDeployed
+	ch <- channelsStarted
+	ch <- messagesReceived
+	ch <- messagesFiltered
+	ch <- messagesQueued
+	ch <- messagesSent
+	ch <- messagesErrored
 }
 
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	e.Lock()
-	defer e.Unlock()
-	err := e.collect()
+	lines, err := e.fetchStatLines()
 	if err != nil {
-		e.up.Set(0)
-		ch <- e.up
+		ch <- prometheus.MustNewConstMetric(
+			up, prometheus.GaugeValue, 0,
+		)
 		log.Error(err)
 		return
 	}
-	e.up.Set(1)
-	ch <- e.up
-	ch <- e.channelsDeployed
-	ch <- e.channelsStarted
-	e.messagesReceived.Collect(ch)
-	e.messagesFiltered.Collect(ch)
-	e.messagesQueued.Collect(ch)
-	e.messagesSent.Collect(ch)
-	e.messagesErrored.Collect(ch)
+	ch <- prometheus.MustNewConstMetric(
+		up, prometheus.GaugeValue, 1,
+	)
+	e.readStatus(lines, ch)
+	e.readChannelStats(lines, ch)
 }
 
-func (e *Exporter) collect() error {
-	err := e.fetchStatLines()
-	if err != nil {
-		return err
-	}
-	e.parseStatus()
-	e.parseChannelStats()
-	return nil
-}
-
-func (e *Exporter) fetchStatLines() error {
+func (e *Exporter) fetchStatLines() ([]string, error) {
 	cmd := exec.Command("java", "-jar", e.jarPath, "-c", e.configPath)
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if err := cmd.Start(); err != nil {
-		return err
+		return nil, err
 	}
 	fmt.Fprintln(stdin, "status")
 	fmt.Fprintln(stdin, "channel stats")
 	stdin.Close()
 	bytesOut, err := ioutil.ReadAll(stdout)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if err := cmd.Wait(); err != nil {
-		return err
+		return nil, err
 	}
-	e.statLines = strings.Split(string(bytesOut), "\n")
-	if len(e.statLines) < 3 {
-		return fmt.Errorf("Unexpected output: %s", string(bytesOut))
+	lines := strings.Split(string(bytesOut), "\n")
+	if len(lines) < 3 {
+		return nil, fmt.Errorf("Unexpected output: %s", string(bytesOut))
 	}
 	log.Debug(string(bytesOut))
-	return nil
+	return lines, nil
 }
 
-func (e *Exporter) parseStatus() {
+func (e *Exporter) readStatus(lines []string, ch chan<- prometheus.Metric) {
 	deployed := regexp.MustCompile(`^[0-9a-f-]{36}\s+[a-zA-Z]+\s+`)
 	started := regexp.MustCompile(`\s+Started\s+`)
 	deployedCount, startedCount := 0, 0
-	for _, line := range e.statLines {
+	for _, line := range lines {
 		if deployed.MatchString(line) {
 			deployedCount++
 			if started.MatchString(line) {
@@ -179,26 +141,40 @@ func (e *Exporter) parseStatus() {
 			}
 		}
 	}
-	e.channelsDeployed.Set(float64(deployedCount))
-	e.channelsStarted.Set(float64(startedCount))
+	ch <- prometheus.MustNewConstMetric(
+		channelsDeployed, prometheus.GaugeValue, float64(deployedCount),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		channelsStarted, prometheus.GaugeValue, float64(startedCount),
+	)
 }
 
-func (e *Exporter) parseChannelStats() {
+func (e *Exporter) readChannelStats(lines []string, ch chan<- prometheus.Metric) {
 	stat := regexp.MustCompile(`^(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(.+)$`)
-	for _, line := range e.statLines {
+	for _, line := range lines {
 		if stat.MatchString(line) {
 			group := stat.FindStringSubmatch(line)
 			channel := group[6]
 			received, _ := strconv.ParseFloat(group[1], 64)
-			e.messagesReceived.WithLabelValues(channel).Set(received)
+			ch <- prometheus.MustNewConstMetric(
+				messagesReceived, prometheus.CounterValue, received, channel,
+			)
 			filtered, _ := strconv.ParseFloat(group[2], 64)
-			e.messagesFiltered.WithLabelValues(channel).Set(filtered)
+			ch <- prometheus.MustNewConstMetric(
+				messagesFiltered, prometheus.CounterValue, filtered, channel,
+			)
 			queued, _ := strconv.ParseFloat(group[3], 64)
-			e.messagesQueued.WithLabelValues(channel).Set(queued)
+			ch <- prometheus.MustNewConstMetric(
+				messagesQueued, prometheus.GaugeValue, queued, channel,
+			)
 			sent, _ := strconv.ParseFloat(group[4], 64)
-			e.messagesSent.WithLabelValues(channel).Set(sent)
+			ch <- prometheus.MustNewConstMetric(
+				messagesSent, prometheus.CounterValue, sent, channel,
+			)
 			errored, _ := strconv.ParseFloat(group[5], 64)
-			e.messagesErrored.WithLabelValues(channel).Set(errored)
+			ch <- prometheus.MustNewConstMetric(
+				messagesErrored, prometheus.CounterValue, errored, channel,
+			)
 		}
 	}
 }
